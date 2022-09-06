1296895102da38041eecb462c15066cd17f7ce5d
vivek <vivekwarkade000@gmail.com>
2022-09-06 23:43:38 +0530

    pydantic - postdb

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+import schemas
+
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message":"Hello! this is vivek"}
+fakeDataBase = {
+    1:{'task':'Maditation'},
+    2:{'task':'Read book'},
+    3:{'task':'Code'},
+}
+@app.get("/{id}")
+def getItems(id:int):
+    return fakeDataBase[id]
+
+
+@app.post("/")
+def addItem(task:str):
+    newId = len(fakeDataBase.keys()) + 1
+    fakeDataBase[newId] = {"task":task}
+    return fakeDataBase
